9578fa7cc2b69a77d46535f8c96bb5f58df236cd
CoreELEC Build <coreelec@build.local>
2026-05-07 22:06:21 +0000

    Update LLVM and librespot recipes

--- packages/addons/service/librespot/package.mk
+++ packages/addons/service/librespot/package.mk
@@ -3,10 +3,10 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="librespot"
-PKG_VERSION="0.7.1"
-PKG_VERSION_DATE="2025-04-01"
-PKG_SHA256="1d09cf7a9b05663bc74806dc729dba818f2f1108728b60ccaac42bb54bf46864"
-PKG_REV="2"
+PKG_VERSION="0.8.0"
+PKG_VERSION_DATE="2025-11-11"
+PKG_SHA256="0e4922997e1c67d27b3f50dcc388ecb8a3c08eba23b764879071f6e9e8c07ec7"
+PKG_REV="0"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/librespot-org/librespot/"
@@ -23,6 +23,12 @@ PKG_ADDON_TYPE="xbmc.service"
 PKG_MAINTAINER="Anton Voyl (awiouy)"
 
 make_target() {
+  # aws-lc-rs fails when CMAKE is set in the environment.
+  unset CMAKE
+  export CMAKE_TOOLCHAIN_FILE="${CMAKE_CONF}"
+  export CMAKE_INSTALL_PREFIX="/usr"
+
+  export BINDGEN_EXTRA_CLANG_ARGS="--sysroot=${SYSROOT_PREFIX}"
   export RUSTC_LINKER=${CC}
   cargo build \
     --target ${TARGET_NAME} \
--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="19.1.7"
-PKG_SHA256="82401fea7b79d0078043f7598b835284d6650a75b93e64b6f761ea7b63097501"
+PKG_VERSION="22.1.5"
+PKG_SHA256="7972b87b705a003ce70ab55f9f0fb495d156887cba0eb296d284731139118e2c"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
 PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
