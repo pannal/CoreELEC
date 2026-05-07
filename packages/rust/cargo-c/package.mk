75140c08f06086c6512b36b72f5ef4faa62d5a8c
CoreELEC Build <coreelec@build.local>
2026-05-07 21:20:46 +0000

    rust: bump cargo-c to 0.10.12

--- packages/rust/cargo-c/package.mk
+++ packages/rust/cargo-c/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2023-present Team CoreELEC (https://coreelec.org)
 
 PKG_NAME="cargo-c"
-PKG_VERSION="v0.9.17"
-PKG_SHA256="95fcf50505516dfa6a941e666d1388810da9d7a9e1c623c09068faef5d50b3b9"
+PKG_VERSION="v0.10.12"
+PKG_SHA256="ae118882067e1e7dcd8106933329cf018ddc6ea56cabfea7642a7699d6ce700f"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/lu-zero/cargo-c"
 PKG_URL="https://github.com/lu-zero/cargo-c/archive/refs/tags/${PKG_VERSION}.tar.gz"
@@ -11,14 +11,10 @@ PKG_DEPENDS_HOST="cargo:host"
 PKG_LONGDESC="Use Cargo-c to build and install C-compatible libraries"
 PKG_TOOLCHAIN="manual"
 
-pre_make_host() {
-  cp -f ${PKG_DIR}/Cargo.lock ${PKG_BUILD}/Cargo.lock
-}
-
 make_host() {
-  cargo build --release --locked --manifest-path ${PKG_BUILD}/Cargo.toml
+  cargo build --release --manifest-path ${PKG_BUILD}/Cargo.toml
 }
 
 makeinstall_host() {
-  cargo install --profile release --locked --path ${PKG_BUILD}
+  cargo install --profile release --path ${PKG_BUILD}
 }
